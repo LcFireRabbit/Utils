2f5432b46926fc9e20179ead77f4ed8f3504fcff
lianglei <1035040216@qq.com>
2023-07-04 16:54:20 +0800

    文件或文件夹名称校验帮助方法

---
+++ Utils/CheckHelper/CheckNameHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.CheckHelper
+{
+    public static class CheckNameHelper
+    {
+        /// <summary>
+        /// 文件或文件夹名称
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static bool CheckFileOrDirectory(string name)
+        {
+            bool isValid = true;
+            string[] strList = new string[] { " ", "/", "\"", @"\", @"\/", ":", "*", "?", "<", ">", "|", "\r\n" };
+
+            if (string.IsNullOrEmpty(name))
+            {
+                isValid = false;
+            }
+            else
+            {
+                foreach (string errStr in strList)
+                {
+                    if (name.Contains(errStr))
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+
+            return isValid;
+        }
+    }
+}
