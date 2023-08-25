2d8713b0351d312cfe2bc70758a6b8c047908f18
lianglei <1035040216@qq.com>
2023-08-25 19:32:32 +0800

    修改

--- Utils/WindowsApi/WMI/Drive/Drive.cs
+++ Utils/WindowsApi/WMI/Drive/Drive.cs
@@ -20,19 +20,22 @@ namespace Utils.WindowsApi
             driveLetter = driveLetter.TrimEnd('\\');
 
             // browse all USB WMI physical disks
-            foreach (ManagementObject drive in new ManagementObjectSearcher("select DeviceID, MediaType,InterfaceType from Win32_DiskDrive").Get())
+            foreach (ManagementObject drive 
+                in new ManagementObjectSearcher("select DeviceID, MediaType,InterfaceType from Win32_DiskDrive")
+                .Get()
+                .Cast<ManagementObject>())
             {
                 // associate physical disks with partitions
                 ManagementObjectCollection partitionCollection = new ManagementObjectSearcher(String.Format("associators of {{Win32_DiskDrive.DeviceID='{0}'}} " + "where AssocClass = Win32_DiskDriveToDiskPartition", drive["DeviceID"])).Get();
 
-                foreach (ManagementObject partition in partitionCollection)
+                foreach (ManagementObject partition in partitionCollection.Cast<ManagementObject>())
                 {
                     if (partition != null)
                     {
                         // associate partitions with logical disks (drive letter volumes)
                         ManagementObjectCollection logicalCollection = new ManagementObjectSearcher(String.Format("associators of {{Win32_DiskPartition.DeviceID='{0}'}} " + "where AssocClass= Win32_LogicalDiskToPartition", partition["DeviceID"])).Get();
 
-                        foreach (ManagementObject logical in logicalCollection)
+                        foreach (ManagementObject logical in logicalCollection.Cast<ManagementObject>())
                         {
                             if (logical != null)
                             {
