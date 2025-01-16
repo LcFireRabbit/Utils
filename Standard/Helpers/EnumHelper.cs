e2ee7b98b137fd0dc4d63234a8de842fccfb25e0
lianglei <1035040216@qq.com>
2025-01-16 17:19:12 +0800

    添加枚举帮助类

---
+++ Standard/Helpers/EnumHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Standard.Helpers
+{
+    public static class EnumHelper
+    {
+        public static ObservableCollection<TEnum> GetEnumList<TEnum>() where TEnum : Enum
+        {
+            var cols = new ObservableCollection<TEnum>();
+            var items = Enum.GetValues(typeof(TEnum));
+            foreach (var item in items)
+            {
+                cols.Add((TEnum)item);
+            }
+            return cols;
+        }
+    }
+}
